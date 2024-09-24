ddd0a6d0cfff2807ec25337c4378ad045f49c63b
RednaxMarc <xander.marcle@hotmail.com>
2024-09-24 21:31:44 +0200

    Removed the pitstop lap from Lando's laptime graph

--- norris_singapore.py
+++ norris_singapore.py
@@ -12,7 +12,6 @@ ns = {'mrd': 'http://ergast.com/mrd/1.5'}
 
 # Get the lap times
 lap_times = [lap.find('.//mrd:Timing',ns).attrib['time'] for lap in root.findall('.//mrd:Lap',ns)]
-print(lap_times)
 
 # Function to convert lap time from 'MM:SS.mmm' to total seconds
 def lap_time_to_seconds(lap_time):
@@ -25,8 +24,20 @@ lap_times_in_seconds = [lap_time_to_seconds(lap_time) for lap_time in lap_times]
 # Generate lap numbers (1, 2, 3, ...)
 lap_numbers = list(range(1, len(lap_times_in_seconds) + 1))
 
+# Get the lap where Lando pitted, and remove that lap from the lap times
+url = 'http://ergast.com/api/f1/2024/last/drivers/norris/pitstops.xml'
+response = requests.get(url)
+root = ET.fromstring(response.content)
+pit_laps = [int(pit_lap.find('.//mrd:PitStop', ns).attrib['lap']) for pit_lap in root.findall('.//mrd:PitStopsList', ns)]
+print(pit_laps)
+for i in pit_laps:
+    lap_times_in_seconds.pop(i-1)
+    lap_numbers.pop(i-1)
+print(len(lap_times_in_seconds))
+print(len(lap_numbers))
+
+
 # Plotting the lap times
-plt.figure(figsize=(10, 6))
 plt.plot(lap_numbers, lap_times_in_seconds, marker='o', linestyle='-', color='b')
 
 # Adding labels and title
@@ -36,6 +47,12 @@ plt.ylabel('Lap Time (seconds)', fontsize=12)
 
 # Show the plot
 plt.grid(True)
+plt.show()
+
+# Make a boxplot of the lap times
+plt.boxplot(lap_times_in_seconds)
+plt.title('Lap Times Boxplot', fontsize=14)
+plt.ylabel('Lap Time (seconds)', fontsize=12)
 plt.show()
 
 
